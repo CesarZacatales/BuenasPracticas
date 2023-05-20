67a53591211c3c8456ca5844fe4b3b5b890b5d07
00218621 <00218621@uca.edu.sv>
2023-05-20 09:25:51 -0600

    V.1.0.1

---
+++ Proyecto_PDM_V.1.0.1/app/src/main/java/com/example/zacatales/smartrobotapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.zacatales.smartrobotapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+}
